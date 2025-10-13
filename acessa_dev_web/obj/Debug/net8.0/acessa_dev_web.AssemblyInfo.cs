2a5218013cff8ff8da9bb065a043725955a45a2a
MRSantos1804 <marcosrodrigo.santos@gmail.com>
2025-10-13 13:00:12 -0300

    AjusteLinkParaPaginaCadastro

--- acessa_dev_web/obj/Debug/net8.0/acessa_dev_web.AssemblyInfo.cs
+++ acessa_dev_web/obj/Debug/net8.0/acessa_dev_web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("acessa_dev_web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f4b6999617a1dccc114227e47cd21270ab4db31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de021ce0820dcbb31a6b7d78335fa893d0c03dd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("acessa_dev_web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("acessa_dev_web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
