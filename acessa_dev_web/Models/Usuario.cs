de021ce0820dcbb31a6b7d78335fa893d0c03dd0
MRSantos1804 <marcosrodrigo.santos@gmail.com>
2025-10-12 22:07:58 -0300

    TableUsuarios

--- acessa_dev_web/Models/Usuario.cs
+++ acessa_dev_web/Models/Usuario.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel.DataAnnotations;
-
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace acessa_dev_web.Models
--- acessa_dev_web/obj/Debug/net8.0/acessa_dev_web.AssemblyInfo.cs
+++ acessa_dev_web/obj/Debug/net8.0/acessa_dev_web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("acessa_dev_web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a53893c3fd3d8d7b09389267bfc1924d819c6886")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f4b6999617a1dccc114227e47cd21270ab4db31")]
 [assembly: System.Reflection.AssemblyProductAttribute("acessa_dev_web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("acessa_dev_web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
