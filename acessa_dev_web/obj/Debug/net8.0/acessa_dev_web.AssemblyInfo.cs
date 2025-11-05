3174fbfc209810af29c4cf17a8a46637559a2489
MRSantos1804 <marcosrodrigo.santos@gmail.com>
2025-11-04 23:17:41 -0300

    Ajuste Enums de edit tela de Usuário e Ocorrência

--- acessa_dev_web/obj/Debug/net8.0/acessa_dev_web.AssemblyInfo.cs
+++ acessa_dev_web/obj/Debug/net8.0/acessa_dev_web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("acessa_dev_web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ea2e3080458eb1459111ace96ed0b0e2d25799a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3a448678af00100471fa43e61d3545e766574af")]
 [assembly: System.Reflection.AssemblyProductAttribute("acessa_dev_web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("acessa_dev_web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
